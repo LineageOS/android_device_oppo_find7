d25bd41ff8fe4b820611ac4446c2a2f0d3aafab6
Michael Bestas <mikeioannina@gmail.com>
2017-03-01 19:04:40 +0200

    find7: Build camera HAL from source
    * Remove wrapper, prebuilt HAL & shims
    
    Change-Id: I7dff6c067c1936e56082b0a0af4b5903bb91a78c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,11 +52,6 @@ TARGET_SYSTEM_PROP += device/oppo/find7/system.prop
 # Recovery
 TARGET_RECOVERY_FSTAB := device/oppo/find7/rootdir/etc/fstab.recovery
 
-# Shims
-TARGET_LD_SHIM_LIBS := \
-    /system/vendor/lib/hw/camera.vendor.msm8974.so|libshim_camera.so \
-    /system/lib/libcamera_client.so|libshim_camera_parameters.so
-
 # TWRP
 ifeq ($(WITH_TWRP),true)
 TARGET_RECOVERY_DEVICE_DIRS += device/oppo/find7/twrp
