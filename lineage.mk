141c5b9f45b2a13e1917c908f51236b44056580c
Simon Shields <simon@lineageos.org>
2017-08-23 00:32:04 +1000

    find7: s@vendor/cm@vendor/lineage@g
    Change-Id: I472b1c9e31e81671bcb9e09f43d5c497395d662a

--- lineage.mk
+++ lineage.mk
@@ -18,8 +18,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from find7 device
 $(call inherit-product, device/oppo/find7/find7.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_find7
 PRODUCT_DEVICE := find7
