c1737b22367a2384b74efeae2242a19aee405a86
jrior001 <jriordan001@gmail.com>
2017-10-19 23:07:19 -0400

    find7: remove CameraParameters header
    Change-Id: I71f4e908972f7060cc2ead888453f6a333c87ca8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,9 +17,6 @@
 # Inherit from msm8974-common
 -include device/oppo/msm8974-common/BoardConfigCommon.mk
 
-# Include path
-TARGET_SPECIFIC_HEADER_PATH += device/oppo/find7/include
-
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
 TARGET_KERNEL_CONFIG := lineageos_find7_defconfig
