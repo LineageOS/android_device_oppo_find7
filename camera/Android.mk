af21ae5b2722407faadcf16301b67836cfdce6b3
Rashed Abdel-Tawab <rashed@linux.com>
2017-11-01 18:21:09 -0700

    find7: CameraWrapper: Include nativebase headers
     * This is required as of android-8.0.0_r23
    
    Change-Id: Ic76cfff3a5bfe46c0ea6974e1ffce9fb7ed069c6

--- camera/Android.mk
+++ camera/Android.mk
@@ -7,6 +7,9 @@ include $(CLEAR_VARS)
 LOCAL_C_INCLUDES := \
     system/media/camera/include
 
+LOCAL_HEADER_LIBRARIES += \
+    libnativebase_headers
+
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
