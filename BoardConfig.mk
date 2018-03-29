cd543f408c2753621a7669d0f491ecf181e8e6d7
Elektroschmock <elektroschmock78@googlemail.com>
2018-03-29 21:04:57 +0200

    find7: Clean up kernel cmdline
    * msm_rtb, depends on CONFIG_MSM_RTB which is not set for bacon
    
    Change-Id: Ie78609a9df945aab556b2ab363469f882cc189bb
    Signed-off-by: Andrea Pucci <linckandrea01@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,7 +20,7 @@
 DEVICE_PATH := device/oppo/find7
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 ehci-hcd.park=3
 TARGET_KERNEL_CONFIG := lineageos_find7_defconfig
 TARGET_KERNEL_SOURCE := kernel/oppo/msm8974
 
