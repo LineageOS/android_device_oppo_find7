11dab73e1baa568a76cd59cfe13e3d0c21a20e3f
padarshr <padarshr@codeaurora.org>
2019-04-25 17:58:17 +0530

    find7: Address 'LOCAL_MODULE_TAGS := eng debug' deprecation.
    Newer AOSP deprecates LOCAL_MODULE_TAGS := eng debug.
    We now remove that and instead add the package to
    PRODUCT_PACKAGES_ENG or PRODUCT_PACKAGES_DEBUG as applicable.
    
    To ensure that we don't miss out on any package during this switch,
    we add these explicitly, even though it might have been already
    added under PRODUCT_PACKAGES at some other location.
    
    Change-Id: I35bd67c3e7752be5bcd8f8682864062173e92724
    (cherry picked from commit 32bfafe895fe7bd1140877a183b42804f5b1465f)

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.qcom
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.qcom
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
@@ -13,7 +13,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
@@ -21,7 +21,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.usb.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.usb.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
